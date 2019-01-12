75c45d4e74d8e8e7e10f5278d89bd89a21d82a91
FX-Wood <fxwood2@gmail.com>
2019-01-11 16:30:08 -0800

    Added support for attacks

--- content.js
+++ content.js
@@ -6,9 +6,18 @@ console.log("hello I'm chromeroller. Click to roll!");
 function addOnClickToSkills() {
 
     //get all skill boxes and assign them to variable "skills"
-    var skills = document.querySelectorAll(".ct-skills__item");
+    let skills = document.querySelectorAll(".ct-skills__item");
     
-    //A function that changes the background of the element it's called on to red
+    //This loops over each skill box and adds an event listener to be executed on "click"
+    //leaves a flag when they are added so that listeners aren't duplicated
+    skills.forEach(function(element){
+        if (!element.iAmListening) {
+            element.addEventListener("click", rollSkillCheck)
+            element.iAmListening = true
+            console.log("Adding onClick listeners to skills")
+        }
+    })
+
     function rollSkillCheck() {
         let modifier = this.querySelector(".ct-signed-number__number").innerText
         let sign = this.querySelector(".ct-signed-number__sign").innerText
@@ -19,7 +28,7 @@ function addOnClickToSkills() {
         roll.open("POST", "https://api.dicemagic.io/roll")
         roll.setRequestHeader("Content-Type", "application/json")
         roll.send(`{"cmd":"1d20${sign}${modifier}"}`)
-        roll.onreadystatechange = function () {
+        roll.onreadystatechange = function() {
             if (roll.readyState === 4) {
             let reply = JSON.parse(roll.responseText)
             let skillCheckResult = reply.result.match(/\*(.*)\*/)[0].slice(1, -1)
@@ -32,27 +41,89 @@ function addOnClickToSkills() {
             }
         }
     }
-    //This loops over each skill box and adds an event listener to be executed on "click"
-    skills.forEach(function(element){
-        if (!element.iAmListening) {
-        element.addEventListener("click", rollSkillCheck)
-        element.iAmListening = true
-        console.log("Adding onClick listeners to skills")
+}
+
+function addOnclickToPrimaryBox() {
+    //checks if the actions tab of the primary box is active
+    if (document.querySelector('.ct-attack-table__content')) {
+        //makes an array of each item on the attack table
+        let attacks = Array.from(document.querySelector('.ct-attack-table__content').children);
+        
+        //adds an event listener and flags each item in the attack table
+        attacks.forEach(attack => {
+            if(!attack.iAmListening) {
+                attack.addEventListener('click', attackAndDamageRoll);
+                attack.iAmListening = true;
+                console.log('Adding listeners to attack table');
+            }
+        })
+
+        function attackAndDamageRoll() {
+            let toHit = this.querySelector('.ct-combat-attack__tohit .ct-signed-number').textContent;
+            console.log(toHit);
+            let damage = this.querySelector('.ct-damage__value').textContent;
+            let damageType = this.querySelector('.ct-tooltip').dataset.originalTitle.toLowerCase();
+            if (damageType === "item is customized") {
+                damageType = "non-mundane"
+            }
+            console.log(damage + ' ' + damageType);
+
+            const roll = new XMLHttpRequest;
+            roll.open("POST", "https://api.dicemagic.io/roll");
+            roll.setRequestHeader("Content-Type", "application/json");
+            roll.send(`{"cmd":"1d20${toHit} ${damage}"}`)
+            console.log(`{"cmd":"1d20${toHit} ${damage}"}`)
+            roll.onreadystatechange = function() {
+                if (roll.readyState === 4) {
+                    reply = JSON.parse(roll.responseText).result;
+                    console.log(reply)
+                    hitResult = reply.match(/\*([0-9]+)\*/g)[0].slice(1, -1)
+                    damageResult = reply.match(/\*([0-9]+)\*/g)[1].slice(1, -1)
+
+                    let resultString = `
+                    You rolled ${hitResult} to hit.
+                    If you strike true: ${damageResult} ${damageType} damage!
+                    `
+
+
+                    alert(resultString)
+                
+                }
+            }
+
+
+                }
+            }
         }
 
-    })
-}
+// function rollDice(command) {
+//     let roll = new XMLHttpRequest;
+//     roll.open("POST", "https://api.dicemagic.io/roll");
+//     roll.setRequestHeader("Content-Type", "application/json");
+//     roll.send(`{"cmd":"${command}"}`)
+//     console.log(`{"cmd":"${command}"}`)
+//     roll.onreadystatechange = function() {
+//         if (roll.readyState === 4) {
+//             console.log("readyState = 4")
+//             console.log(roll.responseText)
+//             return roll.responseText
+//         }
+//     }
+
+// function parseResult(apiResponse) {
+//     let rollObject = JSON.parse(apiResponse)
+//     let rollResult = rollObject.result.match(/\*(.*)\*/)[0].slice(1, -1)
+//     console.log("parsing " + rollResult)
+//     return rollResult
+// }
+
+// function parseRaw(apiResponse) {
+//     let rollObject = JSON.parse(apiResponse);
+//     let rawRoll = rollObject.result.match(/\((\d*)\)/)[1]
+//     console.log("parsing raw " + rawRoll)
+//     return rawRoll
+// }
+
 
 window.setInterval(addOnClickToSkills, 1000)
-
-
-function askDiceMagic(roll="1d20+10") {
-    var url = "https://api.dicemagic.io/roll"
-    var request = new XMLHttpRequest
-    request.open("POST", url)
-    request.setRequestHeader("Content-Type", "application/json")
-    request.send(`{"cmd":${roll}}`)
-    return request
-}
-
-
+window.setInterval(addOnclickToPrimaryBox, 1000)
